a8581238cdbd86b2f3f223ee63a21e14aa5c1e97
Mingzhu Wan <mingzhuwan@gmail.com>
2021-09-25 10:26:52 +1000

    updated index.js and App.js to work with the Update button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      meaningOfLife: 47 + this.props.increment,
+    }
+    // this.props = props;
+  }
+
+  // If there are no props to be used
+  // state = {
+  //   meaningOfLife: 47
+  // }
+
+  handleClick = () => {
+    // this.setState({ meaningOfLife: this.state.meaningOfLife + 1 },
+    //   () => console.log(this.state.meaningOfLife))
+    
+    // this.setState((prevState, prevProps) => {
+    //   return {meaningOfLife: this.state.meaningOfLife + 1}
+    // },
+    //   () => console.log(this.state.meaningOfLife)
+    // )
+
+    // this.setState((prevState, prevProps) => {
+    //   return {meaningOfLife: prevState.meaningOfLife + this.props.increment}
+    // },
+    //   () => console.log(this.state.meaningOfLife)
+    // )
+
+    this.setState((prevState, prevProps) => {
+      return {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
+    },
+      () => console.log(this.state.meaningOfLife)
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button
+            onClick={this.handleClick}
+          >
+            Update State
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
